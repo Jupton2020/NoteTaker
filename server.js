63cd802542439ac40f03080b95c90d4d6d376ff8
jack upt <jhonbahama@gmail.com>
2020-01-10 21:32:23 -0800

    updated delete functionality

--- server.js
+++ server.js
@@ -8,22 +8,13 @@ var app = express();
 var PORT = process.env.PORT ||3000;
 
 // sets up the express app to handle data parsing
-app.use(express.urlencoded({extended: true }));
-app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
-// create random ID for note
-var ID = function(){    
-    return( 
-        "_" + 
-        Math.random()
-        .toString(36)
-        .substr(2,9)
-    );
-};
+
 
 //routes
-//if no matching route is found default to home
 app.get("/",function(req,res) {  
     res.sendFile(path.join(__dirname,"./public/index.html"));
 });
@@ -33,50 +24,48 @@ app.get("/notes",function(req,res) {
 });
 
 app.get("/api/notes",function(req,res){ 
-    var db = fs.readFileSync("./db/db.json","utf8");
-    return res.json(JSON.parse(db));
-})
+     fs.readFile("./db/db.json","utf8",(err,data) =>{   
+         if(err) throw err;
+         console.log(data);
+     res.json(JSON.parse(data));
+    });
+});
 
-//create the array to save all notes
-var notesArray = [];
+
 
 //recieve a new note to save on the request body, add it to the db.json file 
 // and then return the new note to the client.
 app.post("/api/notes",function (req,res){   
-    var newNote = req.body;
-    newNote.id = ID();
-
-    var data = fs.readFileSync("./db/db.json","utf8");
-    notesArray = JSON.parse(data);
-
-    notesArray.push(newNote);
-    var noteJSON = JSON.stringify(notesArray);
-
-    fs.writeFileSync("./db/db.json",noteJSON,"utf8",err =>{ 
-        if (err) throw err;
+   fs.readFile('./db/db.json','utf8',(err,data) => {    
+       if (err) throw err;
+       let temp = JSON.parse(data);
+       console.log(temp)
+       temp.push(req.body)
+       fs.writeFile("./db/db.json",JSON.stringify(temp),(err) => { 
+           if (err) throw err;
+           res.json('The file has been saved')
+        })
     });
-
 });
+
+
 
 //When the trashcan icon is clicked..
 //deletes the targeted note by the object's id in the db json file after a user deletes a note 
-app.delete("/api/notes/:id",function(req,res){  
-  var deleteNote = req.params.id;
+app.delete("/api/notes/:id",function(req,res){ 
+fs.readFile('./db/db.json','utf8',(err,data) => { 
+    if (err) throw err;
+    let temp = JSON.parse(data);
+    temp.splice(req.params.id, 1)
+    fs.writeFile("./db/db.json",JSON.stringify(temp),(err) =>{ 
+        if (err) throw err;
+        res.json('The file has been removed')
+     })
+  })
+});
 
-  notesArray = notesArray.filter(function (note){   
-      return note.id != deleteNote;
-  });
-
-  console.log(notesArray);
-  var deletedNotes = JSON.stringify(notesArray);
-
-  fs.writeFileSync("./db/db.json",deletedNotes,"utf8",err =>{   
-      if (err) throw err;
-  });
-
-
-
-
+app.post("/api/clear",function(req,res){    
+    db.length = 0;
 })
 
 //starts the server to beging listening 
